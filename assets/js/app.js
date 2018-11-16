4644165b3485ccb4ac525e98dd8c8b1f18b8dbcf
Bobby Rae <bobbyleftovers@gmail.com>
2018-11-16 18:10:30 -0500

    readme

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,27 +36,8 @@ Vue.prototype.$_window = window;
  * Startup Vue
  */
 
-//  require('./setup/main.js');
-
 const app = new Vue({
     el: '#app',
     router,
     store
 });
-
-// window.axios.interceptors.response.use(function (response) {
-//     return response;
-// }, function (error) {
-//     switch (error.response.status) {
-//         case 401:
-//             // window.location = '/login/expired'
-//             break;
-
-//         case 419:
-//             // window.location = '/login/expired';
-//             break;
-
-//     }
-
-//     return Promise.reject(error);
-// });
